4bd3df7b5b657103d798a1619021158ef05b627a
Bryan Weinstein <bweinstein@seas.harvard.edu>
2018-01-18 18:31:28 -0500

    Making progress.

---
+++ diffusionPressureFoam.C
@@ -0,0 +1,93 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    simpleFoam
+
+Description
+    Steady-state solver for incompressible, turbulent flow, using the SIMPLE
+    algorithm.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "simpleControl.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "postProcess.H"
+
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createFields.H"
+  
+    while (runTime.run())
+    {
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        //*** Calculate the concentration field ***
+
+        while (simple.correctNonOrthogonal())
+        {
+            fvScalarMatrix cEqn
+            (
+                fvm::ddt(c)
+              - fvm::laplacian(D, c)
+            );
+            cEqn.solve();
+        }
+
+	//*** Calculate the resulting pressure field
+	volScalarField rho("rho", rho_0*(1 + beta*c)); // For consistent notation with bousinesq
+
+	while (simple.correctNonOrthogonal())
+        {
+            fvScalarMatrix pEqn
+            (
+                fvm::grad(P)
+              - rho*g
+            );
+            pEqn.solve();
+        }
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;	
+    }
+
+    Info<< "End\n" << endl;
+    
+    return 0;
+}
+
+
+// ************************************************************************* //
